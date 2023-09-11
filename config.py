b2f1c0a93746359a723a301c118e5ceb9f00e908
Hamdani17 <88165773+Hamdani17@users.noreply.github.com>
2023-09-11 09:49:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ API_HASH = "50214647e873d2a67e9b64b1c8d8faf5"
 BOT_TOKEN = "6656920864:AAGr9mVrmIDDtHGl1R6hTVd-wyxdXA55keU"
 OWNER_ID = '929366169'
 
-MONGO_DB_URI = "MONGO_DB_URI"
+MONGO_DB_URI = "postgres://ipstsvzlcdelhm:18459d8b93c8fc5ad48ab734ae310b479f3cc2b524d7a4d8266c83c84d77c46c@ec2-3-223-151-6.compute-1.amazonaws.com:5432/d7eg59on4cqf6"
 MUST_JOIN = "SoFeThon"
